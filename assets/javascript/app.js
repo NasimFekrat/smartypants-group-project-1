30f65b4c68f7805762168d0a7152c8040a383286
Paraprax <parkerjohnston28@gmail.com>
2018-11-08 19:47:53 -0500

    added links && API key to our firebase database

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
     
+    // connection to our firebase database
+    var config = {
+        apiKey: "AIzaSyDqFP7koygUVZ_6fKkTfW1NvjhhZNPye7s",
+        authDomain: "coffee-finder-app.firebaseapp.com",
+        databaseURL: "https://coffee-finder-app.firebaseio.com/", 
+        storageBucket: "coffee-finder-app.appspot.com"
+      };
+  
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
     
 
 }); // end of docready function
