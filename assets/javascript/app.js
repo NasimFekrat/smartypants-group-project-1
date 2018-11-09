822d995eeca7b3d4de0713c63aa0c9f8362e6a80
David Downie <daviddownie@gmail.com>
2018-11-08 20:03:22 -0500

    added weather api key

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+
+    var APIKey = "2aacadc69f5b8add90497de8f4f7fc24";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=Toronto,Canada&units=imperial&appid=" + APIKey;
     
     
 
