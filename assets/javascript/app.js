a2f516a01d98c7087c1b54a3ed6890659311b153
David Downie <daviddownie@gmail.com>
2018-11-08 20:06:30 -0500

    pulled from master

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
+     
 
-    var APIKey = "2aacadc69f5b8add90497de8f4f7fc24";
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=Toronto,Canada&units=imperial&appid=" + APIKey;
     
     // connection to our firebase database
     var config = {
@@ -14,6 +13,10 @@ $(document).ready(function(){
     firebase.initializeApp(config);
 
     var database = firebase.database();
+
+    //api key for openweathernetwork
+    var APIKey = "2aacadc69f5b8add90497de8f4f7fc24";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=Toronto,Canada&units=imperial&appid=" + APIKey;
     
 
 }); // end of docready function
