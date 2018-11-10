e289097d5ea0b5ea3fa6fbc4604d03d378c04543
Jessa Molyneux <jessa@refineddata.com>
2018-11-10 14:25:34 -0500

    new updates to firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,19 +24,90 @@ $(document).ready(function(){
     var inputCoffeeOrder;
     var inputDestination;
 
-    // Button for adding New Trains
 
-$("#submit-button").on("click", function(event) {
+// Button to add Coffee Fetchers
+$("#coffeeFetcher-button").on("click", function(event) {
     event.preventDefault();
 
 
 // Grabs user input
-
 inputCoffeeFetcher = $("#coffee-fetcher").val().trim();
-inputCoffeeOrder = $("#train-first").val().trim();
-inputTrainFrequency =$("#train-frequency").val().trim();
+inputDestination =$("#coffee-destination").val().trim();
+
+// Creates local "temporary" object for holding Coffee Receiver data
+
+  var CoffeeFetcherInputs = {
+
+    CoffeeFetcher : inputCoffeeFetcher,
+    Destination : inputDestination,
+
+  };
+
+  // Uploads New Input data to the database
+
+database.ref().push(CoffeeFetcherInputs);
+    console.log(CoffeeFetcherInputs.name);
+
+$("coffee-fetcher").val("");
+$("coffee-destination").val("");
+
 
 });
 
+
+
+// Button to add Coffee Receivers
+$("#coffeeReceiver-button").on("click", function(event) {
+    event.preventDefault();
+
+
+// Grabs user input
+inputCoffeeReceiver =$("#coffee-receiver").val().trim();
+inputCoffeeOrder = $("#coffee-order").val().trim();
+
+
+// Creates local "temporary" object for holding Coffee Receiver data
+
+var CoffeeReceiverInputs = {
+
+    CoffeeReceiver : inputCoffeeReceiver,
+    CoffeeOrder : inputCoffeeOrder,
+
+  };
+
+  // Uploads New Input data to the database
+
+database.ref().push(CoffeeReceiverInputs);
+    console.log(CoffeeReceiverInputs.name);
+
+$("coffee-fetcher").val("");
+$("coffee-destination").val("");
+
+});
+
+
+// 3. Create Firebase event for adding Coffee Receiver and Fetcher Inputs to the database and a row in the html when a user adds an entry
+
+database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+  
+    // Store everything into a variable.
+     inputCoffeeFetcher = childSnapshot.val().CoffeeFetcher;
+     inputCoffeeReceiver = childSnapshot.val().CoffeeReceiver;
+     inputDestination = childSnapshot.val().Destination;
+     inputCoffeeOrder = childSnapshot.val().CoffeeOrder;
+
+
+     var newRow = $("<tr>").append(
+        $("<td>").text(inputCoffeeFetcher),
+        $("<td>").text(inputCoffeeReceiver),
+        $("<td>").text(inputCoffeeOrder),
+        $("<td>").text(inputDestination),
+    )
+
+    $("#trainTable >tbody").append(newRow);
+
+
+});
 
 }); // end of docready function
