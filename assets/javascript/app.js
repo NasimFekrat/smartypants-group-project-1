a5eff4adbbe5e932e8fe1c599dc64a44eecd31f4
Jessa Molyneux <jessa@refineddata.com>
2018-11-10 14:22:11 -0500

    ls

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -18,7 +18,25 @@ $(document).ready(function(){
     var APIKey = "2aacadc69f5b8add90497de8f4f7fc24";
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + "q=Toronto,Canada&units=imperial&appid=" + APIKey;
     
-    // test line for jessa
-    // merged
+    // Initial Values
+    var inputCoffeeFetcher;
+    var inputCoffeeReceiver;
+    var inputCoffeeOrder;
+    var inputDestination;
+
+    // Button for adding New Trains
+
+$("#submit-button").on("click", function(event) {
+    event.preventDefault();
+
+
+// Grabs user input
+
+inputCoffeeFetcher = $("#coffee-fetcher").val().trim();
+inputCoffeeOrder = $("#train-first").val().trim();
+inputTrainFrequency =$("#train-frequency").val().trim();
+
+});
+
 
 }); // end of docready function
